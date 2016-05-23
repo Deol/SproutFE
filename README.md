ab8b491f4a0375053f442f830f6b7327736c5db5
Aeo <A_doz@126.com>
2016-05-23 16:16:27 +0800

    initial

---
+++ README.md
@@ -0,0 +1,44 @@
+# SproutFE
+
+##1. 环境要求
+
+* `node`
+
+##2. 使用说明
+*  进入项目根目录，运行 `npm install` 安装所有依赖。
+*  运行 `gulp server` 进行本地调试开发。
+*  运行 `gulp build` 打包上线代码。
+
+##3. 项目结构
+```
+|
++- node_modules      // 项目依赖，引入的第三方库或框架
++- src               // 开发目录
+|   +- index.html    // 入口文件
+|   +- sidebar.html
+|   +- assets		 // 自己的样式和脚本
+|		+- css/
+|		+- js/
+|		+- images/
+|   +- less
++- dist              // 产出目录
+|   +- index.html
+|   +- public
+|		+- js/
+|		+- css/
+|		+- images/
+```
+
+## 4. 项目中引入依赖
+
+**第三方 `js` 或者 `css` 文件**
+
+> 在 `*.html` 直接引入 `/node_modules/**/*.js` 或 `/node_modules/**/*.css` 即可。
+
+**引入自定义的样式和脚本**
+
+> 在 `*.html` 直接引入 `/assets/css/*.css` 或 `/assets/js/*.js` 。
+
+##5. 发布
+* 进入项目根目录，在终端中运行 `gulp build`。
+
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var del = require('del');
+var less = require('gulp-less');
+var htmlmin = require('gulp-htmlmin');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var gulpif = require('gulp-if');
+var sourcemaps = require('gulp-sourcemaps');
+var lessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new lessAutoprefix({browsers: ['last 2 versions'], cascade: false});
+
+// 删除 css
+gulp.task('clean:css', function () {
+    del.sync('./src/assets/css/*.css');
+})
+
+// 启动本地服务
+gulp.task('connect', function () {
+    connect.server({
+        root: './src/',
+        port: 8081,
+        livereload: true
+    });
+});
+
+gulp.task('connect:build', function () {
+    connect.server({
+        root: './dist/',
+        port: 8080,
+        livereload: true
+    });
+})
+
+// 编译 less
+gulp.task('less', function () {
+    return gulp.src('./src/assets/less/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./src/assets/css/'))
+	    .pipe(connect.reload());
+});
+
+// 处理 js
+gulp.task('script', function () {
+    return gulp.src('./src/assets/js/*.js')
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+    return gulp.src(['./src/views/**/*.html'])
+        .pipe(connect.reload());
+})
+
+// 监控文件
+gulp.task('watch', function () {
+    gulp.watch('./src/assets/less/*.less', ['less']);
+    gulp.watch(['./src/*.html', './src/views/**/*.html'], ['html']);
+    gulp.watch(['./src/assets/js/**/*.js'], ['script']);
+});
+
+gulp.task('clean:build', function () {
+    del.sync('dist/', {force: true});
+});
+
+gulp.task('copy', function () {
+    gulp.src('./src/assets/fonts/**')
+        .pipe(gulp.dest('./dist/assets/fonts/'));
+
+    gulp.src('./src/assets/images/**')
+        .pipe(gulp.dest('./dist/assets/images/'));
+});
+
+gulp.task('minify', ['clean:build', 'less'], function () {
+    gulp.src('src/views/**/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist/views'));
+
+    gulp.src('src/index.html')
+        .pipe(usemin({
+            libjs: [],
+            js: [uglify({mangle: false}), rev()],
+            css: [minifyCss(), 'concat', rev()]
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+// 本地开发
+gulp.task('server', ['less', 'connect', 'watch']);
+
+// build 上线代码
+gulp.task('build', ['clean:build', 'copy', 'minify']);
+
+gulp.task('default', ['server']);
